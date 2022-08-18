0c9353f3691a09bafbd39a640206e7ad8ce71e74
praveen <praveenpkbadiger@gmail.com>
2022-08-18 22:16:53 +0530

    Created Simple linkedList

---
+++ bridgelab/LinkedList.java
@@ -0,0 +1,33 @@
+package bridgelab;
+
+public class LinkedList<T>{
+
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+
+    }
+
+    public void show() {
+        if(head == null){
+            System.out.println("Linked list Empty");
+        }
+        else {
+            Node<T> temp = head;
+            while (temp != null) {
+                System.out.println(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+}
